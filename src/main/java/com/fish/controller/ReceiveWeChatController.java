42c3b3245fc0ea993be9705989e996a5b6da2e75
w1441639547 <1441639547@qq.com>
2018-05-31 16:01:46 +0800

    配置接收消息推送

--- src/main/java/com/fish/controller/ReceiveWeChatController.java
+++ src/main/java/com/fish/controller/ReceiveWeChatController.java
@@ -2,12 +2,14 @@ package com.fish.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/receiveWeChat")
 public class ReceiveWeChatController {
 
 	@RequestMapping("/receive")
+	@ResponseBody
 	public String receive(String echostr) {
 		return echostr;
 	}
